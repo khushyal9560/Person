eadf882074454a983d227bdf95a85e975bb5ef69
khushyal <tomarshiva25@gmail.com>
2024-07-30 21:41:58 +0530

    all files saves

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
-
+require('dotenv').config();
 // Define the MongoDB conncetions URL
-const mongoURL = "mongodb://localhost:27017/hotels";
+// const mongoURL = process.env.MongoDB_URL_LOCAL;
+ const mongoURL=process.env.MongoDB_URL;
 
 // set up MongoDB conncetion
 mongoose.connect(mongoURL, {
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
+require("dotenv").config();
 
 const bodyparser = require("body-parser");
 app.use(bodyparser.json()); // req.body stored post request
+const PORT = process.env.PORT || 8000;
 
 app.get("/", function (req, res) {
   return res.send("Welcome to my server ...");
